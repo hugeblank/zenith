a60f62e4a392f9d6a4a79779f964392145ca46f4
lumiscosity <averyrudelphe@gmail.com>
2024-11-21 17:07:31 +0100

    backport: 1.14
    works all the way up to 1.15.2

--- src/main/java/dev/hugeblank/asahi/client/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/dev/hugeblank/asahi/client/mixin/ClientPlayNetworkHandlerMixin.java
@@ -3,7 +3,7 @@ package dev.hugeblank.asahi.client.mixin;
 import dev.hugeblank.asahi.client.TimeSmoother;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.network.packet.s2c.play.WorldTimeUpdateS2CPacket;
+import net.minecraft.client.network.packet.WorldTimeUpdateS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,7 +15,7 @@ public abstract class ClientPlayNetworkHandlerMixin {
     
     @Shadow private ClientWorld world;
 
-    @Inject(at=@At(value="INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;setTime(J)V"), method="onWorldTimeUpdate(Lnet/minecraft/network/packet/s2c/play/WorldTimeUpdateS2CPacket;)V", cancellable = true)
+    @Inject(at=@At(value="INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;setTime(J)V"), method="onWorldTimeUpdate(Lnet/minecraft/client/network/packet/WorldTimeUpdateS2CPacket;)V", cancellable = true)
     private void clearTickable(WorldTimeUpdateS2CPacket packet, CallbackInfo ci) {
         ((TimeSmoother) world).asahi$updateTimes(packet);
         this.world.setTime(packet.getTime());
